2efe637553aea06e7753b43cd9261df03949d8e9
Gabriel Rodriguez <rodriel.ant@gmail.com>
2018-09-05 00:21:55 -0500

    This one actually works

--- chessBoard.js
+++ chessBoard.js
@@ -1,15 +1,17 @@
+
+
 const chessBoard = {
   board: '',
   makeABoard: function() {
-    for (let i = 0; i <= 7; i++) {
-      for (let a = 0; a <= 7; a++) {
-        this.board += a % 0 === i % 0 ? " ": "#"
-          if (a % 2 === i % 2) {
+    for (let i = 0; i < 8; i++) {
+      for (let a = 0; a < 8; a++) {
+        this.board += (a % 2) === (i % 2) ? " ": "#"
+        /*  if (a % 2 === i % 2) {
             this.board += " "
           } else {
             this.board += "#"
-              };
-          }
+              }; */
+              }
         this.board += "\n";
         }
       this.showBoard();
